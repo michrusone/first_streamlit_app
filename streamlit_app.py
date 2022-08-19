c4ac1cfb72f05b2e1dfbad051bf6cb28ae711049
michrusone <58323694+michrusone@users.noreply.github.com>
2022-08-19 12:59:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('1 Position')
-streamlit.text('2 Position')
-streamlit.text('3 Position')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
