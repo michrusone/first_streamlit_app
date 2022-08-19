4f96dc21b34e69c9b22ed0cf7cf82986e49d33ab
michrusone <58323694+michrusone@users.noreply.github.com>
2022-08-19 12:57:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My parents new healthy dinner')
+streamlit.title('My parents New Healthy Dinner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('1 Position')
+streamlit.text('2 Position')
+streamlit.text('3 Position')
+
